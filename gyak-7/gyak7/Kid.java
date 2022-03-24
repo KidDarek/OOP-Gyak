dff2f9480961d6527165f7cbab7349cb248cfa06
KidDarek <90865592+KidDarek@users.noreply.github.com>
2022-03-24 09:32:35 +0100

    Add files via upload

--- gyak-7/gyak7/Kid.java
+++ gyak-7/gyak7/Kid.java
@@ -1,5 +1,28 @@
 package gyak7;
 
-public class Kid {
+public class Kid extends Person {
+	
+	private String school;
+
+	public Kid(String name, int age, String school) {
+		super(name, age);
+		this.school = school;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return super.toString() + " - " + school;
+	}
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+	
 
 }
--- gyak-7/gyak7/Parent.java
+++ gyak-7/gyak7/Parent.java
@@ -1,5 +1,29 @@
 package gyak7;
 
-public class Parent {
+public class Parent extends Person {
+	
+	private String workplace;
+
+	public Parent(String name, int age, String workplace) {
+		super(name, age);
+		this.workplace = workplace;
+		
+	}
+	
+	@Override
+	public String toString()
+	{
+		return super.toString() + " - " + workplace;
+	}
+
+	public void setWorkplace(String workplace) {
+		this.workplace = workplace;
+	}
+
+	public String getWorkplace() {
+		return workplace;
+	}
+	
+	
 
 }
